a6757b655126765a3af4ceb360bc475313dd1d04
RoyVX <roygreenvx@sina.com>
2019-01-23 15:40:01 +0800

    数据处理

--- js/detail.js
+++ js/detail.js
@@ -1,80 +1,152 @@
-layui.use(['element', 'table','laydate'], function () {
+layui.use(['element', 'table', 'laydate'], function () {
     var element = layui.element;
     var table = layui.table;
     var laydate = layui.laydate;
+    var $ = layui.jquery;
 
-  laydate.render({
-      elem: '#select-timebegin'
-  });
-
-  laydate.render({
-      elem: '#select-timeend'
-  });
-  
-    table.render({
-      elem: '#test'
-      , cols: [[ //标题栏
-        { field: 'date', title: '日期' }
-        , { field: 'index', title: '首页' }
-        , { field: 'hgjj', title: '宏观经济' }
-        , { field: 'gjsd', title: '国际视点' }
-        , { field: 'jrzg', title: '金融中国' }
-        , { field: 'zdztsjk', title: '重点专题数据库' }
-        , { field: 'qqjj', title: '全球经济' }
-      ]]
-      , data: [{
-        "date": "2018年1月"
-        , "index": "12345"
-        , "hgjj": "23456"
-        , "gjsd": "345"
-        , "jrzg": "567"
-        , "zdztsjk": "5678"
-        , "qqjj": "5678"
-      }]
-      //,skin: 'line' //表格风格
-      //, even: true
-      //,page: true //是否显示分页
-      //,limits: [5, 7, 10]
-      //,limit: 5 //每页默认显示的数量
+    laydate.render({
+        elem: '#select-timebegin'
     });
-  
-  
-  
+
+    laydate.render({
+        elem: '#select-timeend'
+    });
+
+    table.render({
+        elem: '#test'
+        , id: "analyseTable"
+        , limit: 20
+        , loading: true
+        , cols: [[ //标题栏
+            { "field": "首页", "title": "首页" },
+            { "field": "综合频道", "title": "综合频道" },
+            { "field": "宏观频道", "title": "宏观频道" },
+            { "field": "金融频道", "title": "金融频道" },
+            { "field": "行业频道", "title": "行业频道" },
+            { "field": "区域频道", "title": "区域频道" },
+            { "field": "国际频道", "title": "国际频道" },
+            { "field": "为您服务", "title": "为您服务" },
+            { "field": "财经视频", "title": "财经视频" }
+        ]]
+        , data: []
+        //,skin: 'line' //表格风格
+        //, even: true
+        //,page: true //是否显示分页
+        //,limits: [5, 7, 10]
+        //,limit: 5 //每页默认显示的数量
+    });
+
+
+
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('chart'));
-  
-    // 指定图表的配置项和数据
-    var option = {
-      title: {
-        text: '浏览次数柱状图'
-      },
-      tooltip: {},
-      // legend: {
-      //   type: 'scroll',
-      //   orient: 'vertical',
-      //   left: '1%',
-      //     top: '10%',
-      //   data: ['首页','宏观经济','国际视点','金融中国', '重点专题数据库','全球经济']
-      // },
-      grid: {
-        left: '4%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        data: ['首页','宏观经济','国际视点','金融中国', '重点专题数据库','全球经济']
-      },
-      yAxis: {},
-      series: [{
-        //name: '首页',
-        type: 'bar',
-        //stack: '浏览量',
-        data: [12345, 23542, 28954, 8951, 7889, 6954]
-      }
-      ]
-    };
-  
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-  });
+
+
+    $("#btn-search").on("click", function () {
+        var timestart = $("#select-timebegin").val();
+        var timeend = $("#select-timeend").val();
+        var ipstart = $("#input-ipbegin").val();
+        var ipend = $("#input-ipend").val();
+        
+        var origindata = [
+            {
+                "fdID": "-1",
+                "fdName": "首页",
+                "fdCount": 221
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559c4d-0001",
+                "fdName": "综合频道",
+                "fdCount": 4
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559cf9-0003",
+                "fdName": "宏观频道",
+                "fdCount": 7
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559d28-0005",
+                "fdName": "金融频道",
+                "fdCount": 2
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559db4-0009",
+                "fdName": "区域频道",
+                "fdCount": 0
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559dd3-000b",
+                "fdName": "国际频道",
+                "fdCount": 0
+            },
+            {
+                "fdID": "4028c7ca-37115425-0137-11559d66-0007",
+                "fdName": "行业频道",
+                "fdCount": 4
+            },
+            {
+                "fdID": "4b4ff4a5-3b1cfb0e-013b-2618ac6d-2d1c",
+                "fdName": "财经视频",
+                "fdCount": 0
+            },
+            {
+                "fdID": "4b4ff4a4-39b8f896-0139-b90e8442-01d0",
+                "fdName": "为您服务",
+                "fdCount": 2
+            }
+        ];
+
+        var tableheader = [[]];
+        var tabledata=[{}];
+        var chartheader = [];
+        var chartdata = [];
+        for (var i = 0; i < origindata.length; i++) {
+            var tempheader = {};
+            tempheader.field = origindata[i].fdName;
+            tempheader.title = origindata[i].fdName;
+            tableheader[0].push(tempheader);
+            tabledata[0][origindata[i].fdName]=origindata[i].fdCount;
+            chartheader.push(origindata[i].fdName);
+            chartdata.push(origindata[i].fdCount);
+        }
+
+        //alert(JSON.stringify(tableheader));
+        //alert(JSON.stringify(tabledata));
+
+        table.reload("analyseTable", {
+            loading: true,
+            cols: tableheader,
+            data: tabledata
+        })
+
+        //alert(JSON.stringify(chartdata));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: '浏览次数柱状图'
+            },
+            tooltip: {},
+            grid: {
+                left: '4%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis: {
+                data: chartheader
+            },
+            yAxis: {},
+            series: [{
+                //name: '首页',
+                type: 'bar',
+                //stack: '浏览量',
+                data: chartdata
+            }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+    })
+});
--- js/index.js
+++ js/index.js
@@ -1,83 +1,26 @@
 layui.use(['element', 'table'], function () {
     var element = layui.element;
     var table = layui.table;
+    var $ = layui.jquery;
 
     table.render({
         elem: '#test'
-        , cols: [[ //标题栏
-            { field: 'date', title: '日期' }
-            , { field: 'index', title: '首页' }
-            , { field: 'hgjj', title: '宏观经济' }
-            , { field: 'gjsd', title: '国际视点' }
-            , { field: 'jrzg', title: '金融中国' }
-            , { field: 'zdztsjk', title: '重点专题数据库' }
-            , { field: 'qqjj', title: '全球经济' }
+        , id: "analyseTable"
+        , limit: 20
+        , loading: true
+        , cols: [[
+            { "field": "fdDate", "title": "日期" },
+            { "field": "首页", "title": "首页" },
+            { "field": "综合频道", "title": "综合频道" },
+            { "field": "宏观频道", "title": "宏观频道" },
+            { "field": "金融频道", "title": "金融频道" },
+            { "field": "行业频道", "title": "行业频道" },
+            { "field": "区域频道", "title": "区域频道" },
+            { "field": "国际频道", "title": "国际频道" },
+            { "field": "为您服务", "title": "为您服务" },
+            { "field": "财经视频", "title": "财经视频" }
         ]]
-        , data: [{
-            "date": "2018年1月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年2月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年3月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年4月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年5月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年6月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年7月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }, {
-            "date": "2018年8月"
-            , "index": "12345"
-            , "hgjj": "23456"
-            , "gjsd": "345"
-            , "jrzg": "567"
-            , "zdztsjk": "5678"
-            , "qqjj": "5678"
-        }]
+        , data: []
         //,skin: 'line' //表格风格
         //, even: true
         //,page: true //是否显示分页
@@ -90,73 +33,14 @@ layui.use(['element', 'table'], function () {
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('chart'));
 
-    // 指定图表的配置项和数据
-    var option = {
-        title: {
-            text: '浏览次数柱状图'
-        },
-        tooltip: {},
-        legend: {
-            type: 'scroll',
-            orient: 'vertical',
-            left: '1%',
-            top: '10%',
-            data: ['首页', '宏观经济', '国际视点', '金融中国', '重点专题数据库', '全球经济']
-        },
-        grid: {
-            left: '15%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            data: ["2018年1月", "2018年2月", "2018年3月", "2018年4月", "2018年5月", "2018年6月", "2018年7月", "2018年8月"]
-        },
-        yAxis: {},
-        series: [{
-            name: '首页',
-            type: 'bar',
-            stack: '浏览量',
-            data: [12345, 23542, 28954, 8951, 7889, 6954, 10001, 9999]
-        }, {
-            name: '宏观经济',
-            type: 'bar',
-            stack: '浏览量',
-            data: [6549, 8526, 4785, 9465, 4625, 7856, 8546, 6549]
-        }, {
-            name: '国际视点',
-            type: 'bar',
-            stack: '浏览量',
-            data: [6549, 8526, 4785, 9465, 4625, 7856, 8546, 6549]
-        }, {
-            name: '金融中国',
-            type: 'bar',
-            stack: '浏览量',
-            data: [6549, 8526, 4785, 9465, 4625, 7856, 8546, 6549]
-        }, {
-            name: '重点专题数据库',
-            type: 'bar',
-            stack: '浏览量',
-            data: [6549, 8526, 4785, 9465, 4625, 7856, 8546, 6549]
-        }, {
-            name: '全球经济',
-            type: 'bar',
-            stack: '浏览量',
-            data: [6549, 8526, 4785, 9465, 4625, 7856, 8546, 6549]
-        }
-        ]
-    };
 
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-
-    $("#btn-search").on("click",function(){
+    $("#btn-search").on("click", function () {
         var year = $(".select-type").val();
-        if(year==""){
+        if (year == "") {
             alert("请选择年份！");
             return;
         }
-        var origindata=[
+        var origindata = [
             {
                 "fdDate": "2019年1月",
                 "首页": 221,
@@ -315,13 +199,72 @@ layui.use(['element', 'table'], function () {
             }
         ];
 
-        var tableheader=[[]];
-        for(var i=0;i<origindata.length;i++){
-            for(var jobject in origindata[i]){
-                var tempjson={};
-                tempjson.field=jobject;
-                tempjson.title=jobject;
+        var tableheader = [[]];
+        var chartheader = [];
+        var chartdata = [];
+        for (var i = 0; i < origindata.length; i++) {
+            if (i == 0) {
+                for (var jobject in origindata[i]) {
+                    var tempjson = {};
+                    tempjson.field = jobject;
+                    if (tempjson.field == "fdDate") {
+                        tempjson.title = "日期";
+                    }
+                    else {
+                        tempjson.title = jobject;
+                        var tempchart = {};
+                        tempchart.name = jobject;
+                        tempchart.type = "bar";
+                        tempchart.stack = "浏览量";
+                        tempchart.data = [];
+                        chartdata.push(tempchart);
+                        chartheader.push(jobject);
+                    }
+                    tableheader[0].push(tempjson);
+                }
+            }
+            if(i < origindata.length-1){
+                for (var j = 0; j < chartdata.length; j++) {
+                    chartdata[j].data.push(origindata[i][chartdata[j].name]);
+                }
             }
         }
+
+        table.reload("analyseTable", {
+            loading: true,
+            cols: tableheader,
+            data: origindata
+        })
+
+        //alert(JSON.stringify(chartdata));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: '浏览次数柱状图'
+            },
+            tooltip: {},
+            legend: {
+                type: 'scroll',
+                orient: 'vertical',
+                left: '1%',
+                top: '10%',
+                data: chartheader
+            },
+            grid: {
+                left: '15%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis: {
+                data: ["2018年1月", "2018年2月", "2018年3月", "2018年4月", "2018年5月", "2018年6月", "2018年7月", "2018年8月", "2018年9月", "2018年10月", "2018年11月", "2018年12月"]
+            },
+            yAxis: {},
+            series: chartdata
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
     })
 });
